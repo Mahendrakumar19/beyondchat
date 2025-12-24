4f6746351fa8db80bbee46988c8f343e3c56eea7
Mahendra Kumar <mahendra.mk174@gmail.com>
2025-12-25 00:26:49 +0530

    Docker file update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+        'php' => phpversion(),
+        'env' => app()->environment()
+    ]);
 });
